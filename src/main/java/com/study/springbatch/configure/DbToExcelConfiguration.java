3ae9c432840d28141c1da62281a43c23c38d5200
dukim <sajacall82@gmail.com>
2024-10-22 01:34:52 +0900

    ref

--- src/main/java/com/study/springbatch/configure/DbToExcelConfiguration.java
+++ src/main/java/com/study/springbatch/configure/DbToExcelConfiguration.java
@@ -16,7 +16,6 @@ import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
